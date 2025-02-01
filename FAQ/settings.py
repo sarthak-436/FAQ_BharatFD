eaf5cae3f18c8a60b1abd5e7bf573fb5afb97a48
sarthak436 <khandelwalsarthak491@gmail.com>
2025-02-01 19:42:12 +0530

    ckeditor and templates

--- FAQ/settings.py
+++ FAQ/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'faqs',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -117,8 +118,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        'height': 300,
+        'width': '100%',
+    },
+}
