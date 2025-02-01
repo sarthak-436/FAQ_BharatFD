7ba4a0b0446712eb7eee089ddcdd48de162f555b
sarthak436 <khandelwalsarthak491@gmail.com>
2025-02-01 20:09:19 +0530

    Translation in Model

--- faqs/models.py
+++ faqs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from googletrans import Translator
 
 class FAQ(models.Model):
     # Default language (English)
@@ -13,3 +14,24 @@ class FAQ(models.Model):
     answer_bn = RichTextField(blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def get_translated_question(self, lang):
+        return getattr(self, f'question_{lang}', None) or self.question
+    
+    # Translate to different languages
+    # For now, only Hindi and Bengali are taken
+    
+    def save(self, *args, **kwargs):
+        translator = Translator()
+        languages = ['hi', 'bn']
+        
+        for lang in languages:
+            # Translate question if empty
+            if not getattr(self, f'question_{lang}'):
+                try:
+                    translated_q = translator.translate(self.question, dest=lang).text
+                    setattr(self, f'question_{lang}', translated_q)
+                except:
+                    pass
+        
+        super().save(*args, **kwargs)
--- faqs/views.py
+++ faqs/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import FAQ
 
+class FAQViewSet(viewsets.ModelViewSet):
+    
+    queryset = FAQ.objects.all()
+    
 
