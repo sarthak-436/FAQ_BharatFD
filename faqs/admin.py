8f72b52d33acea6d858923ee20400c28d01b774e
sarthak436 <khandelwalsarthak491@gmail.com>
2025-02-01 19:49:31 +0530

    Model and SuperUser

--- faqs/admin.py
+++ faqs/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import FAQ
 
-# Register your models here.
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'created_at')
+    fieldsets = (
+        ('English', {
+            'fields': ('question', 'answer')
+        }),
+        ('Hindi', {
+            'fields': ('question_hi', 'answer_hi')
+        }),
+        ('Bengali', {
+            'fields': ('question_bn', 'answer_bn')
+        }),
+    )
--- faqs/models.py
+++ faqs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
+class FAQ(models.Model):
+    # Default language (English)
+    question = models.TextField()
+    answer = RichTextField()
 
+    # Translations (Hindi and Bengali)
+    question_hi = models.TextField(blank=True)
+    question_bn = models.TextField(blank=True)
+    answer_hi = RichTextField(blank=True)  # Added Hindi answer
+    answer_bn = RichTextField(blank=True)  # Added Bengali answer
+
+    created_at = models.DateTimeField(auto_now_add=True)
